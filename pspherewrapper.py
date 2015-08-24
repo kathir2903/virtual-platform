dc07ec6712bde20de841573c4937ec45b65bd418
kathir2903 <kathir.gitit@gmail.com>
2015-08-24 00:05:18 -0700

    wip

--- pspherewrapper.py
+++ pspherewrapper.py
@@ -8,11 +8,24 @@ from psphere import managedobjects
 from psphere import errors
 from getconfig import Config
 
-#class pspherewrapper(object):
+class pspherewrapper(object):
+
+    def __init__(self, cfg):
+        self.handle = None
+        self.server = None
+        self.timeout = None
+
+    def connect(self, server, timeout=300):
+        assert self.server is not None
+        assert self.timeout is not None
+
 def init(cfg, args):
     print cfg
     print "*************"
     print args
+    x = pspherewrapper(cfg)
+    print dir(x)
+    
 def get_credentials(vibaseurl):
     #res = urlparse.urlsplit(vibaseurl)
     #hostname = '%s:%d' % (res.hostname, res.port)
